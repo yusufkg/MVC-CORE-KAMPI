c03680ad47d2fa70df8f15835d0eb4555b518596
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-03-04 15:00:19 +0300

    v1

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using CoreDemo.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -76,6 +78,38 @@ namespace CoreDemo.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult WriterAdd(AddProfileImage p) 
+        {
+            Writer w = new Writer();
+            if (p.WriterImage!=null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                w.WriterImage = newimagename;
+            }
+            w.WriterMail = p.WriterMail;
+            w.WriterName = p.WriterName;
+            w.WriterPassword = p.WriterPassword;
+            w.WriterStatus = p.WriterStatus;
+            w.WriterAbout = p.WriterAbout;
+
+            wm.TAdd(w);
+            return RedirectToAction("Index", "Dashboard");
+        }
+
 
     }
 }
